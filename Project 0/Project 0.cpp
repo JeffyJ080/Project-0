35f17bae1cc53de241908605cec2492d27152f93
Eduan Leighton <leightoneduan@gmail.com>
2024-10-04 14:39:52 +0200

    for loop

--- Project 0/Project 0.cpp
+++ Project 0/Project 0.cpp
@@ -4,5 +4,12 @@ using namespace std;
 int main()
 {
     cout << "Hello Jeffy\n";
+    for (int i = 0; i < 10; i++)
+    {
+		cout << "Enter a number: ";
+        cin >> i;
+
+		cout << "You entered: " << i << endl;
+    }
 
 }
